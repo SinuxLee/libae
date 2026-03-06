dffcd79d7136941875ce6af6d4bd3772f822550f
sinuxlee <pingfan14@gmail.com>
2026-03-06 13:25:39 +0800

    fix Windows build: improve MSVC Makefile and add MinGW Makefile.win
    Restructure Makefile.mk with proper CFLAGS, dependency tracking, and /nologo.
    Add Makefile.win for building with MinGW/GCC on Windows using IOCP backend.
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-opencode)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- Makefile.mk
+++ Makefile.mk
@@ -1,27 +1,31 @@
 SOURCE = src/ae.c src/anet.c src/sockcompat.c
 LIB_TARGET = libae.lib
-LIBS = ws2_32.lib User32.lib libae.lib
+LIBS = ws2_32.lib libae.lib
 LINK_FLAG = /DEBUG /subsystem:console
+CFLAGS = /EHsc /D_WIN32 /D_MSC_VER /I. /Isrc /W3 /nologo
 
-$(LIB_TARGET):
-	@cl /EHsc /D_WIN32 /c $(SOURCE)
-	@link -lib *.obj /out:$(LIB_TARGET)
-	
-timer:
-	@cl /c example\timer.c
-	@link $(LINK_FLAG) timer.obj $(LIBS) /OUT:timer.exe
-	
-echoserver: example\echoserver.c
-	@cl /c example\echoserver.c
-	@link $(LINK_FLAG) echoserver.obj $(LIBS) /OUT:echoserver.exe
+# ae.c includes ae_iocp.c on Windows, so ae.obj depends on both
+ae.obj: src/ae.c src/ae_iocp.c
+	cl $(CFLAGS) /c src/ae.c /Foae.obj
+anet.obj: src/anet.c
+	cl $(CFLAGS) /c src/anet.c /Foanet.obj
+sockcompat.obj: src/sockcompat.c
+	cl $(CFLAGS) /c src/sockcompat.c /Fosockcompat.obj
 
-echoclient: example\echoclient.c
-	@cl /c example\echoclient.c
-	@link $(LINK_FLAG) echoclient.obj $(LIBS) /OUT:echoclient.exe
+$(LIB_TARGET): ae.obj anet.obj sockcompat.obj
+	link -lib ae.obj anet.obj sockcompat.obj /out:$(LIB_TARGET) /nologo
+
+timer: $(LIB_TARGET) example\timer.c
+	cl $(CFLAGS) /c example\timer.c
+	link $(LINK_FLAG) timer.obj $(LIBS) /OUT:timer.exe /nologo
+
+echoserver: $(LIB_TARGET) example\echoserver.c
+	cl $(CFLAGS) /c example\echoserver.c
+	link $(LINK_FLAG) echoserver.obj $(LIBS) /OUT:echoserver.exe /nologo
+
+echoclient: $(LIB_TARGET) example\echoclient.c
+	cl $(CFLAGS) /c example\echoclient.c
+	link $(LINK_FLAG) echoclient.obj $(LIBS) /OUT:echoclient.exe /nologo
 
 clean:
-	@del *.exe
-	@del *.obj
-	@del *.lib
-	@del *.ilk
-	@del *.pdb
+	-del /Q *.exe *.obj *.lib *.ilk *.pdb 2>nul
